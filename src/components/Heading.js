da99c448347523422a72374357a58dfe21f95435
prasaddds <durgaprasad.78257@gmail.com>
2022-07-18 14:28:59 +0530

    first commit

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -1,6 +1,19 @@
 import React from "react";
 import { heading } from "../styles/homeStyles";
 import { motion } from "framer-motion";
-const Heading = () => <h1 style={{height:"100%",width:"100%",backgroundColor:"black",display:"flex",justifyContent:"center",border:"solid 2px red",boxSizing:"border-box"}}>Games</h1>;
+import "./HeadingStyle.css";
+const Heading = () => <h1 style=
+    {{
+        height: "100%",
+        width: "100%",
+        backgroundColor: "black",
+        display: "flex",
+        justifyContent: "center",
+        border: "solid 2px red",
+        boxSizing: "border-box",
+        
+    }}>
+    Games
+</h1>;
 
 export default Heading;
--- src/components/List2.js
+++ src/components/List2.js
@@ -1,43 +1,43 @@
 import React, { useEffect, useState } from "react";
 import './style1.css';
-const List2=()=>{
-    const [gamelist,setGamelist]=useState([]);
-    const getGameData=async()=>{
-        const response=await fetch('https://s3-ap-southeast-1.amazonaws.com/he-public-data/gamesarena274f2bf.json')
-        // console.log(response);
-        const gameData=await response.json();
-        const gameDone=gameData.slice(1,99)
-        setGamelist(gameDone);
+const List2 = () => {
+    const [initial, set] = useState([]);
+    const functionForAPICall = async () => {
+        const request = await fetch('https://s3-ap-southeast-1.amazonaws.com/he-public-data/gamesarena274f2bf.json')
+        
+        const json1 = await request.json();
+        const gameDone = json1.slice(1, 99)
+        set(gameDone);
     }
-    useEffect(()=>{
-        getGameData();
-    },[])
-    const [searchterm,setSearchterm]=useState('')
-    return(
-        <div className="class1">
-            <div style={{justifyContent:"center"}}>
-                <input type='text' placeholder="Search For Game" onChange={event=>{setSearchterm(event.target.value)}} 
-                style={{display:"flex",height:"100%",width:"100%",justifyContent:"center",marginTop:"10%",boxSizing:"border-box",padding:"2%",borderRadius:"15%"}}/>
+    useEffect(() => {
+        functionForAPICall();
+    }, [])
+    const [enter, fun] = useState('')
+    return (
+        <div className="class1" style={{backgroundColor:"black"}}>
+            <div style={{ justifyContent: "center" }}>
+                <input type='text' placeholder="Search For Game" onChange={event => { fun(event.target.value) }}
+                    style={{ display: "flex", height: "100%", width: "100%", justifyContent: "center", marginTop: "10%", boxSizing: "border-box", padding: "2%", borderRadius: "15%" }} />
             </div>
 
             <div>
-                {gamelist.filter((game)=>{
-                    if(setSearchterm===""){
+                {initial.filter((game) => {
+                    if (fun === "") {
                         return game
                     }
-                    else if(game.title.toLowerCase().includes(searchterm.toLowerCase())){
+                    else if (game.title.toLowerCase().includes(enter.toLowerCase())) {
                         return game;
                     }
-                }).map((game,key)=>{
-                    return(
+                }).map((game, key) => {
+                    return (
                         <div className="card" key={key}>
-                  <h1>{game.title}</h1> <br/> <br/> <br/> <br/> <br/>
-              
-              <p>Platform: {game.platform}</p>
-              <p>Score: {game.score}</p>
-              <p>Genre: {game.genre}</p>
-              <p>Editors Choice: {game.editors_choice}</p>
-          </div>
+                            <h1>{game.title}</h1> <br /> <br /> <br /> <br /> <br />
+
+                            <p>Platform: {game.platform}</p>
+                            <p>Score: {game.score}</p>
+                            <p>Genre: {game.genre}</p>
+                            <p>Editors Choice: {game.editors_choice}</p>
+                        </div>
                     )
                 })}
             </div>
